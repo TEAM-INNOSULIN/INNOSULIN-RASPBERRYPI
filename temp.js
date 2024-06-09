3479be568addd930b9e7c342937505fae8adf349
sam971114 <simonana@naver.com>
2024-06-09 17:54:09 +0900

    feat: temp.js 추가

---
+++ temp.js
@@ -0,0 +1,103 @@
+const fs = require('fs');
+const mqtt = require('mqtt');
+const { Gpio } = require('onoff');
+var GPIO = require('onoff').Gpio;
+const schedule = require('node-schedule');
+const readline = require('readline');
+
+// Load certificates and keys
+const caFile = fs.readFileSync('/home/admin/certs/AmazonRootCA1.pem');
+const certFile = fs.readFileSync('/home/admin/certs/device.pem.crt');
+const keyFile = fs.readFileSync('/home/admin/certs/private.pem.key');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+//sensor
+var alertPin = new GPIO(535, 'out'); //23번 pin
+
+
+// MQTT options including the SSL configuration
+const options = {
+  host: 'a3mwllq937a5i6-ats.iot.us-east-1.amazonaws.com',
+  port: 8883,
+  protocol: 'mqtts',
+  ca: caFile,
+  cert: certFile,
+  key: keyFile
+};
+
+// Connect to the AWS IoT
+const client = mqtt.connect(options);
+
+client.on('connect', () => {
+  console.log('Connected to AWS IoT');
+  client.subscribe('walkingData', { qos: 0 });
+  alertPin.writeSync(0);
+  //publishData();
+  // 키보드 입력 받기
+  rl.setPrompt('혈당 수치를 입력하세요 (종료하려면 Ctrl+C): ');
+  rl.prompt();
+  rl.on('line', function (data) {
+    // Enter를 누를 때마다 메시지 publish
+    const message = JSON.stringify({ value: data });
+    client.publish('bloodSugar', message, { qos: 1 }, (error) => {
+      if (error) {
+        console.error('메시지 전송 실패:', error);
+      }
+    });
+
+    console.log(`혈당 수치 '${data}'가 bloodSugar 토픽으로 전송되었습니다.`);
+    rl.prompt(); // 다시 입력 받기
+  });
+});
+
+
+client.on('message', (topic, message) => {
+    if (topic === 'walkingData') {
+      const data = JSON.parse(message.toString());
+      if (data.needMoreStep === true) {
+        alertPin.writeSync(1); // Turn on the GPIO pin
+        setTimeout(() => {
+          alertPin.writeSync(0); // Turn off the GPIO pin after 3 seconds
+        }, 3000);
+      }
+    }
+});
+
+client.on('error', (error) => {
+  console.log('Connection failed:', error);
+});
+
+
+//keep the Node.js process running even if the publishing function is idle
+process.on('SIGINT', function() {
+    console.log("Caught interrupt signal");
+    rl.close();
+    client.end(true, () => {
+      process.exit();
+    });
+  });
+
+/*
+// Function to read input from the keypad (simulated by the command line)
+function readKeypadInput() {
+  rl.question('Enter data (Press # to finish): ', (input) => {
+    if (input.endsWith('#')) {
+      // Remove the last character '#' and prepare the message
+      const message = input.slice(0, -1); // remove the last character
+      client.publish('bloodSugar', JSON.stringify({ value: message }), { qos: 0, retain: false });
+      console.log('Data published:', message);
+      rl.close();
+    } else {
+      console.log('Data must end with # to finish input.');
+      readKeypadInput(); // Ask for input again
+    }
+  });
+}
+
+// Trigger the input reading function
+readKeypadInput();
+*/
